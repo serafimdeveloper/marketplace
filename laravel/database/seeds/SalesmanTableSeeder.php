c78e468b7125a9b12d9605f920266e991cb14784
Bruno Site <contato@brunosite.com>
2017-02-07 01:14:37 -0200

    Factories adicionadas

---
+++ laravel/database/seeds/SalesmanTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Model\Salesman;
+use Illuminate\Database\Seeder;
+
+class SalesmanTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Salesman::class, 1)->create();
+    }
+}
