f145cd49e33c46885b4caf56fbe3f7a01047f2e0
Bruno Site <contato@brunosite.com>
2017-02-07 18:37:25 -0200

    novas factories

---
+++ laravel/database/seeds/StoreTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Model\Store;
+use Illuminate\Database\Seeder;
+
+class StoreTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Store::class, 1)->create();
+    }
+}
