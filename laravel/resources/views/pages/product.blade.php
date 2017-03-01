657b192ed254f78d9d4fa21959bff94e35e21f65
Bruno Site <contato@brunosite.com>
2017-03-01 09:30:12 -0300

    - novas correções na homeController - página de produtos - página de mensagens

--- laravel/resources/views/pages/product.blade.php
+++ laravel/resources/views/pages/product.blade.php
@@ -31,7 +31,7 @@
                             <p>{{$product->store->name}}</p>
                         </div>
                         <div class="fl-right">
-                            <a class="btn btn-popmartin-trans {{ isset($user) ? 'jq-new-message' : 'jq-auth' }}">
+                            <a class="btn btn-popmartin-trans {{ isset($auth) ? 'jq-new-message' : 'jq-auth' }}">
                                 <i class="fa fa-comments-o"></i> contatar o vendedor
                             </a>
                         </div>
