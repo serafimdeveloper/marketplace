5cf42427fef74981c7b564cadb5643d79f920c2c
Bruno Site <contato@brunosite.com>
2017-03-03 05:32:30 -0300

    - Inicinado checkout2

--- laravel/app/Http/Controllers/CheckoutController.php
+++ laravel/app/Http/Controllers/CheckoutController.php
@@ -14,26 +14,39 @@ class CheckoutController extends Controller{
     public function order(){
         try {
             $customer = $this->moip->customers()->setOwnId(uniqid())
-                ->setFullname('Fulano de Tal')
-                ->setEmail('rendaok@live.com')
+                ->setFullname('Jose Silva')
+                ->setEmail('sandbox_v2_1401147277@email.com')
                 ->setBirthDate('1988-12-30')
-                ->setTaxDocument('22222222222')
+                ->setTaxDocument('33333333333')
                 ->setPhone(11, 66778899)
-                ->addAddress('BILLING',
-                    'Rua de teste', 123,
-                    'Bairro', 'Sao Paulo', 'SP',
-                    '01234567', 8)
                 ->addAddress('SHIPPING',
-                    'Rua de teste do SHIPPING', 123,
-                    'Bairro do SHIPPING', 'Sao Paulo', 'SP',
-                    '01234567', 8);
+                    'Avenida Faria Lima', 2927,
+                    'Itaim', 'Sao Paulo', 'SP',
+                    '01234000', 8);
 
-            $order = $this->moip->orders()->setOwnId(uniqid())
-                ->addItem('Bicicleta Specialized Tarmac 26 Shimano Alivio', 1, 'uma linda bicicleta', 10000)
+            $order1 = $this->moip->orders()->setOwnId(uniqid())
+                ->addItem('Camisa Verde e Amarelo - Brasil', 1, 'Seleção Brasileira', 10000)
+                ->setShippingAmount(100)
                 ->setCustomer($customer)
+                ->addReceiver('MPA-VB5OGTVPCI52');
+
+            $order2 = $this->moip->orders()->setOwnId(uniqid())
+                ->addItem('Camisa Preta - Alemanha', 1, 'Camisa da Copa 2014', 10000)
+                ->setShippingAmount(100)
+                ->setCustomer($customer)
+                ->addReceiver('MPA-IFYRB1HBL73Z');
+
+            $multiorder = $this->moip->multiorders()
+                ->addOrder($order1)
+                ->addOrder($order2)
+                ->setOwnId(uniqid())
                 ->create();
 
-            dd($order);
+//            dd($multiorder);
+//            $multipayments = $this->moip->multiorders()->get('MOR-8H8VSF36G5HT')->multipayments()->execute();
+
+            $multpayment = $multiorder->multipayments();
+            dd($multpayment);
         } catch (Exception $e) {
             dd($e->__toString());
         }
