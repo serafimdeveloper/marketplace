1caec27790e6a60599bcbe746e6635a1cf9ea0ca
agencia.asiw <agencia.asiw@gmail.com>
2017-03-08 15:39:58 -0300

    implementação de busca de produtos destaques e novos

--- laravel/app/Repositories/Accont/ProductsRepository.php
+++ laravel/app/Repositories/Accont/ProductsRepository.php
@@ -57,30 +57,37 @@ class ProductsRepository extends BaseRepository
 
 
     public function getBestSellers(array $with){
-        $model = $this->model->with($with)->distinct()
-            ->select(DB::raw('products.*, SUM(product_request.quantity) AS qtd_product_request'))
+       /* $model = $this->model->with($with)->distinct()
+            ->select(DB::raw('products.*'))
             ->leftJoin('product_request','products.id','=','product_request.product_id')
             ->groupBy('id')
-            ->orderBy('qtd_product_request','desc')
+            ->orderBy(DB::raw('SUM(product_request.quantity)'),'desc')
             ->get();
-        return $model;
+        return $model;*/
+       $model = $this->model->with($with)->get();
+       $collection = $model->sortByDesc(function($product){
+           return $product->requests->each(function($request){
+               return count($request->pivot->quantity);
+           });
+       });
+       return $collection->splice(15);
     }
 
     public function getMostVisited(array $with){
-        $model = $this->model->with($with)->distinct()
-            ->select(DB::raw('products.*, SUM(visit_products.count) AS qtd_product_visit'))
-            ->leftJoin('visit_products','products.id','=','visit_products.product_id')
-            ->groupBy('id')
-            ->orderBy('qtd_product_visit','desc')
-            ->get();
-        return $model;
+        $model = $this->model->with($with)->distinct()->get();
+        $collection = $model->sortByDesc(function($product){
+            return $product->visitproduct->each(function ($visit){
+                return count($visit->count);
+            });
+        });
+        return $collection->splice(15);
     }
 
     public function getHighlights(array $with){
         $collection = $this->getBestSellers($with);
         $all = $collection->merge($this->getMostVisited($with));
         if($all->count() >= 10){
-            $all = $all->random(10);
+            $all = $all->random(20);
         }
         return $all->all();
     }
