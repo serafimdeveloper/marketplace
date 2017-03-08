0e18e1170307733ce056944b99dab0b5f1c7214d
Bruno Site <contato@brunosite.com>
2017-03-08 06:53:53 -0300

    - integração moip parte 12 (Moip connect realizado e persistido)

--- laravel/app/Model/ConnectSallesman.php
+++ laravel/app/Model/ConnectSallesman.php
@@ -4,10 +4,9 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ConnectSallesman extends Model
-{
-    use SoftDeletes;
+class ConnectSallesman extends Model{
 
+    protected $table = 'connect_sallesmans';
     protected $fillable = ['salesman_id', 'accessToken', 'access_token', 'refreshToken', 'refresh_token', 'scope', 'moipAccount_id', 'expires_in'];
 
     public function salesman()
