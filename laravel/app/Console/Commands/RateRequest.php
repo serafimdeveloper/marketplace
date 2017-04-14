ddd22ee50171671c464b4a54285137a286096827
Bruno Site <contato@brunosite.com>
2017-04-14 12:06:45 -0300

    Atualizaçao de rate!

---
+++ laravel/app/Console/Commands/RateRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Request;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class RateRequest extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:rate';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verifica o prazo limite de avaliação para cada pedido';
+    protected $rateRequest;
+
+    /**
+     * Create a new command instance.
+     * RateRequest constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request){
+        parent::__construct();
+        $this->rateRequest = $request;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(){
+        $now = Carbon::now();
+        $limit = $now->subDays(5);
+        $orders = $this->rateRequest
+            ->where('reuquest_status_id', '=', 5)
+            ->where('update_at', '<', $limit)->get();
+        if(count($orders)){
+            $orders->each(function($order){
+                if(!$order->shopvaluation){
+                    $order->shopvaluation()->fill([
+                       'user_id' => $order->user_id,
+                       'store_id' => $order->store_id,
+                       'note_product' => 5,
+                       'note_attendance' => 5,
+                       'comment' => 'Vendedor rápido e atencioso. Recomendo!',
+                       'active' => 1
+                    ]);
+                }
+                Log::info('Avaliação do pedido: ' . $order->key . ' | hora:'. Carbon::now());
+            });
+            Log::info('Avaliação do pedido:  | hora:'. Carbon::now());
+        }
+    }
+}
