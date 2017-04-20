7e082a08730e31aa1beaed88826ad25271600f37
agencia.asiw <agencia.asiw@gmail.com>
2017-04-20 00:44:24 -0300

    Implementação do cadastro de banners

--- laravel/resources/views/layouts/parties/alert_banner.blade.php
+++ laravel/resources/views/layouts/parties/alert_banner.blade.php
@@ -2,32 +2,17 @@
     <div class="alertbox-container">
         <span class="alertbox-close"><i class="fa fa-close fontem-18"></i></span>
         <div class="alertbox-content">
-            <h2 class="alertbox-title c-pop fontw-500">Cadastrar banner</h2>
+            <h2 class="alertbox-title c-pop fontw-500">{{isset($result) ? 'Editar Banner' : 'Cadastrar Banner'}}</h2>
             @if(isset($result))
                 {!! Form::model($result,['class' => 'form-modern pop-form form-banner', 'route' =>['accont.banner.update',$result->id], 'method' => 'POST']) !!}
             @else
                 {!! Form::open(['class' => 'form-modern pop-form form-banner', 'route' => ['accont.banner.store'], 'method' => 'POST']) !!}
             @endif
                 <div class="colbox">
-                    <div class="colbox-2">
+                    <div class="colbox-full">
                         <label>
                            <span>Loja</span>
                            {!! Form::select('store_id',$stores,null,['placeholder' => 'Selecione uma loja']) !!}
-                        </label>
-                    </div>
-                    <div class="colbox-2">
-                        <label>
-                            <span>Título do banner</span>
-                            {!! Form::text('title', null, ['placehoder' => 'Título do banner']) !!}
-                        </label>
-
-                    </div>
-                </div>
-                <div class="colbox">
-                    <div class="colbox-full">
-                        <label>
-                            <span>Url da loja</span>
-                            {!!  Form::text('url', null, ['placehoder' => 'url da loja']) !!}
                         </label>
                     </div>
                     <div class="colbox-full">
@@ -41,19 +26,19 @@
                     <div class="colbox-2">
                         <label>
                             <span>Início</span>
-                            {!! Form::input('datetime-local', 'date_start',  null, ['class' => 'datetimepicker_datetime', 'placehoder' => 'data e hora inicial']) !!}
+                            {!! Form::input('datetime-local', 'date_start',  isset($result->date_start) ? $result->date_start->format('Y-m-d H:i:s') : null, ['class' => 'datetimepicker_datetime', 'placehoder' => 'data e hora inicial']) !!}
                         </label>
                     </div>
                     <div class="colbox-2">
                         <label>
                             <span>Fim</span>
-                            {!! Form::input('datetime-local', 'date_end',  null, ['class' => 'datetimepicker_datetime', 'placehoder' => 'data e hora final']) !!}
+                            {!! Form::input('datetime-local', 'date_end',  isset($result->date_end) ? $result->date_end->format('Y-m-d H:i:s') : null, ['class' => 'datetimepicker_datetime', 'placehoder' => 'data e hora final']) !!}
                         </label>
                     </div>
                 </div>
                 <div class="clear-both"></div>
                 <div class="txt-center">
-                    <button type="submit" class="btn btn-popmartin">cadastrar</button>
+                    <button type="submit" class="btn btn-popmartin">{{isset($result) ? 'atualizar' : 'cadastrar'}}</button>
                 </div>
             {!! Form::close() !!}
         </div>
